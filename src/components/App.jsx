ecdb17ffe2c7cfebfa28369debca104326e508f5
Borys-Grinberg <borys.grinberg@gmail.com>
2024-05-11 00:40:03 +0200

    handler

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+import { useSelector } from 'react-redux';
+import { selectError, selectIsLoading } from 'store/selector';
+
+const App = () => {
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        flexDirection: 'column',
+        justifyContent: 'start',
+        alignItems: 'start',
+        paddingLeft: '24px',
+        fontSize: 18,
+        color: '#010101',
       }}
     >
-      React homework template
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      {isLoading && <div>Loading...</div>}
+      {error}
+      <ContactList />
     </div>
   );
 };
+
+export default App;
